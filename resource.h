5290b00a5246ff4550c99946b46e706c666947b5
Sandro <Sandro@b60034a5-fb9b-f04b-b957-2f3957cf9ea0>
2004-08-27 15:44:36 +0000

    Added OPML export

--- resource.h
+++ resource.h
@@ -48,13 +48,15 @@
 #define ID_VIEW_OPTIONS                 32809
 #define ID_ACTIONS_UPDATEFEEDS          32810
 #define ID_VIEW_CUSTOMIZETOOLBAR        32812
+#define ID_FILE_EXPORT_OPML             32814
+#define ID_FILE_IMPORT_OPML             32815
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        212
-#define _APS_NEXT_COMMAND_VALUE         32813
+#define _APS_NEXT_COMMAND_VALUE         32816
 #define _APS_NEXT_CONTROL_VALUE         1018
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
